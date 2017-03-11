5e88c10b59349eb8a94406d40d1eb5cef4ce54b3
maulanasly <gi.creatorz@gmail.com>
2017-03-11 22:00:52 +0700

    add user_roles model and fixtures

--- api/model/users.py
+++ api/model/users.py
@@ -9,3 +9,11 @@ class UsersModel(db.Model):
     email = db.Coloumn(db.String(100), nullable=False)
     role_id = db.Coloumn(db.Integer, db.ForeignKey('user_roles.id'), nullable=False)
     user_roles = db.relationship('user_roles', foreign_key=[role_id])
+
+
+class UserRoles(db.Model):
+    __table_name__ = "user_roles"
+
+    id = db.Coloumn(db.Integer, primary_key=True)
+    role_name = db.Coloumn(db.String(50), nullable=False, default="Author")
+
