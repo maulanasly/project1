bf12dee5602cb0f54a95b2482fac58c0ae5e9a5a
maulanasly <gi.creatorz@gmail.com>
2017-03-11 22:20:18 +0700

    add more data testing

--- api/model/blog.py
+++ api/model/blog.py
@@ -1,5 +1,7 @@
 from api import db
 
+import sqlalchemy
+
 
 class BlogModel(db.Model):
     __table_name__ = 'blogs'
@@ -8,8 +10,8 @@ class BlogModel(db.Model):
     title = db.Coloumn(db.String(200), nullable=False, index=True)
     author_id = db.Coloumn(db.Integer, db.ForeignKey('users.user_id'))
     content = db.Coloumn(db.String(500), nullable=False)
-    publish_date = db.Coloumn(db.TimeStamp, nullable=False)
-    last_update = db.Coloumn(db.TimeStamp, nullable=False)
+    publish_date = db.Coloumn(db.TimeStamp, nullable=False, default=sqlalchemy.func.current_timestamp())
+    last_update = db.Coloumn(db.TimeStamp,  nullable=False, default=sqlalchemy.func.current_timestamp(), onupdate=sqlalchemy.func.current_timestamp())
     category_id = db.Coloumn(db.Integer, db.ForeignKey('category.id'))
 
     users = db.relationship("UserModel", foreign_keys=[author_id])
@@ -23,5 +25,5 @@ class CategoryModel(db.Model):
     __table_name__ = 'category'
     id = db.Coloumn(db.Integer, primary_key=True, index=True)
     category_name = db.Coloumn(db.String(100))
-    last_update = db.Coloumn(db.Timestamp)
+    last_update = db.Coloumn(db.Timestamp,  nullable=False, default=sqlalchemy.func.current_timestamp(), onupdate=sqlalchemy.func.current_timestamp())
 
